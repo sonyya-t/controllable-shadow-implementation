62e25c24b851487829b4447c2d9f2555e0c229ed
sonyya-t <sonyya.t@gytworkz.com>
2025-10-03 15:18:21 +0530

    remove visualization

--- controllable_shadow/utils/__init__.py
+++ controllable_shadow/utils/__init__.py
@@ -4,10 +4,8 @@ Utility functions for controllable shadow generation.
 
 from .image_processor import ImageProcessor
 from .metrics import ShadowMetrics
-from .visualization import ShadowVisualization
 
 __all__ = [
     "ImageProcessor",
-    "ShadowMetrics", 
-    "ShadowVisualization"
+    "ShadowMetrics"
 ]
